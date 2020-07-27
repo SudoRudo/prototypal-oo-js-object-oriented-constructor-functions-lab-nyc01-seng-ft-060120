be56f1cb5ed911ad20a1ea4a313a92fb2bd88206
Rodolfo  Guerrero-Martinez II <r.guerreronyc@gmail.com>
2020-07-27 04:34:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-function scooter(year, color, model){
+function Scooter(year, color, model){
   this.year = year;
   this.color = color;
   this.model = model;
 }
 
-function driver(name, age, experience){
+function Driver(name, age, experience){
   this.name = name;
   this.age = age;
   this.experience = experience;
 }
 
-function pickUpLocation(address, city){
+function PickUpLocation(address, city){
   this.address = address;
   this.city = city;
 }
